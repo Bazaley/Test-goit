af07b91e51171ae8075aca948bce57d54df16993
Дмитрий <bazaley-dmitriy@i.ua>
2023-07-09 02:15:06 +0300

    delete loader

--- src/components/Tweets/Tweets.jsx
+++ src/components/Tweets/Tweets.jsx
@@ -8,20 +8,19 @@ import {
 } from 'redux/users/users-operations';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectLimit, selectIsLoading } from 'redux/selectors';
+import { selectLimit } from 'redux/selectors';
 import { useLocation } from 'react-router-dom';
 import { Dropdown } from 'components/Dropdown/Dropdown';
 import { clearUsers } from 'redux/users/usersSlice';
-import { Circles } from 'react-loader-spinner';
 
-import { Back, LoadMore, Box, BoxCircles } from './Tweets.styled';
+import { Back, LoadMore, Box } from './Tweets.styled';
 
 const Tweets = () => {
   const [page, setPage] = useState(1);
   const [value, setValue] = useState('');
 
   const limit = useSelector(selectLimit);
-  const loading = useSelector(selectIsLoading);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -55,26 +54,19 @@ const Tweets = () => {
         <Back to={useLocation().state.from}>Go back</Back>
         <Dropdown setValue={setValue} value={value} />
       </Box>
-      {loading ? (
-        <BoxCircles style={{ width: '100px', margin: '0 auto' }}>
-          <Circles color="#4fa94d" visible={true} />
-        </BoxCircles>
-      ) : (
-        <>
-          <UsersList />
 
-          {limit !== 3 ||
-            (!value && (
-              <LoadMore
-                onClick={() => {
-                  setPage(page + 1);
-                }}
-              >
-                Load more
-              </LoadMore>
-            ))}
-        </>
-      )}
+      <UsersList />
+
+      {limit !== 3 ||
+        (!value && (
+          <LoadMore
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            Load more
+          </LoadMore>
+        ))}
     </>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,2 @@
 export const selectUsers = state => state.users.users;
 export const selectLimit = state => state.users.limit;
-export const selectIsLoading = state => state.users.isLoading;
