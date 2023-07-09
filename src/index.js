645e9a64f76bbae7175611db4aeef6ef9d711eb3
Дмитрий <bazaley-dmitriy@i.ua>
2023-07-09 19:48:26 +0300

    fix avatar

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename="/test-goit/">
+        <BrowserRouter basename="/tessdf/">
           <App />
         </BrowserRouter>
       </Provider>
